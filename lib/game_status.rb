4f129fa3ae9d446acb44e6debb9aa8c40ec488d9
mattwecht <mattwecht@gmail.com>
2018-05-30 15:28:12 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -33,7 +33,10 @@ def full? (board)
 end
 
 def draw? (board)
-  if !(won?(board)) #&& full?(board)
+  if (won?(board) != false)
+    return false
+  end
+  if won?(board) && full?(board)
     return true
   end
   return true
