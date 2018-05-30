486138ccc9a45c72c01c3cc2f70359bad8ba8cb6
mattwecht <mattwecht@gmail.com>
2018-05-30 15:08:00 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,7 +20,7 @@ def won? (board)
     positions[board[combo[0]],board[combo[1]],board[combo[2]]] #create an array of values in the positions required for a win
     if(positions[0]==positions[1]&& positions[1]==positions[2]) #if they match
       return combo
-    else 
-      return false
+    end
   end
+  return false #no match found
 end
