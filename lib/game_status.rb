94718b6e8565d21ba0fb353ff69f586e18e1d30f
mattwecht <mattwecht@gmail.com>
2018-05-30 15:09:19 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,7 +15,7 @@ WIN_COMBINATIONS = [
   [1,4,7]
 ]
 def won? (board)
-  WIN_COMBINATIONS.each |combo| do
+  WIN_COMBINATIONS.each do |combo| 
     positions[board[combo[0]],board[combo[1]],board[combo[2]]] #create an array of values in the positions required for a win
     if(positions[0]==positions[1]&& positions[1]==positions[2]) #if they match
       return combo
